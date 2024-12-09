5cecdb75484de1635b5b6cbf0bd31bfbe8d34c6a
Andi Irsandi Ramadani <andi@dbo.id>
2024-12-09 22:02:43 +0700

    fix(product): product management should be able works in offline mode

--- src/app/settings/products/use-products.js
+++ src/app/settings/products/use-products.js
@@ -8,16 +8,10 @@ import {
   collection,
   getCountFromServer,
   onSnapshot,
-  addDoc,
   serverTimestamp,
-  setDoc,
   doc,
-  deleteDoc,
-  // doc,
-  // setDoc,
-  // collectionGroup,
 } from "firebase/firestore";
-import { firestore } from "utils/firebase";
+import { dbDELETE, dbPOST, dbUPDATE, firestore } from "utils/firebase";
 import { useDisclosure } from "@chakra-ui/react";
 import useCloudinaryUpload from "custom-hooks/upload-file";
 import useCustomToast from "custom-hooks/toast";
@@ -37,6 +31,7 @@ const useProducts = (initialPerPage = 10) => {
   const [perPage, setPerpage] = useState(() => initialPerPage);
 
   const { workbench: wb } = useOutletContext();
+  const { uploadFile } = useCloudinaryUpload();
 
   const {
     isOpen: addNew,
@@ -47,11 +42,9 @@ const useProducts = (initialPerPage = 10) => {
       setProduct(null);
     },
   });
+
   const ref = `users/${wb.owner}/workbench/${wb.id}/products`;
 
-  // user/{owner identifier}/workbench/{workbench id}/products
-
-  // Calculate total pages based on the total count of products matching the search
   const calculateTotalPages = async (search) => {
     const countQuery = search
       ? query(
@@ -65,7 +58,6 @@ const useProducts = (initialPerPage = 10) => {
     setTotalPages(Math.ceil(snapshot.data().count / perPage));
   };
 
-  // Fetch products with pagination and optional search
   const fetchProducts = async (page = 1, search = "") => {
     setIsLoading(true);
 
@@ -78,7 +70,6 @@ const useProducts = (initialPerPage = 10) => {
     );
 
     if (page > 1 && pageSnapshots[page - 2]) {
-      // Start after the last document of the previous page
       productQuery = query(productQuery, startAfter(pageSnapshots[page - 2]));
     }
 
@@ -86,7 +77,6 @@ const useProducts = (initialPerPage = 10) => {
       const docs = snap.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
       setProducts(docs);
 
-      // Store the last document snapshot for future pages
       if (snap.docs.length > 0) {
         const newPageSnapshots = [...pageSnapshots];
         newPageSnapshots[page - 1] = snap.docs[snap.docs.length - 1];
@@ -95,54 +85,6 @@ const useProducts = (initialPerPage = 10) => {
     });
     setIsLoading(false);
   };
-
-  // const ref = "products";
-
-  // // Calculate total pages based on the total count of products matching the search
-  // const calculateTotalPages = async (search) => {
-  //   const countQuery = search
-  //     ? query(
-  //         collectionGroup(firestore, ref),
-  //         where("name", ">=", search),
-  //         where("name", "<=", search + "\uf8ff"),
-  //       )
-  //     : collectionGroup(firestore, ref);
-
-  //   const snapshot = await getCountFromServer(countQuery);
-  //   setTotalPages(Math.ceil(snapshot.data().count / perPage));
-  // };
-
-  // // Fetch products with pagination and optional search
-  // const fetchProducts = async (page = 1, search = "") => {
-  //   setIsLoading(true);
-
-  //   let productQuery = query(
-  //     collectionGroup(firestore, ref),
-  //     orderBy("name"),
-  //     where("name", ">=", search),
-  //     where("name", "<=", search + "\uf8ff"),
-  //     limit(perPage),
-  //   );
-
-  //   if (page > 1 && pageSnapshots[page - 2]) {
-  //     // Start after the last document of the previous page
-  //     productQuery = query(productQuery, startAfter(pageSnapshots[page - 2]));
-  //   }
-
-  //   onSnapshot(productQuery, (snap) => {
-  //     const docs = snap.docs.map((doc) => doc.data());
-  //     setProducts(docs);
-
-  //     // Store the last document snapshot for future pages
-  //     if (snap.docs.length > 0) {
-  //       const newPageSnapshots = [...pageSnapshots];
-  //       newPageSnapshots[page - 1] = snap.docs[snap.docs.length - 1];
-  //       setPageSnapshots(newPageSnapshots);
-  //     }
-  //   });
-
-  //   setIsLoading(false);
-  // };
 
   useEffect(() => {
     calculateTotalPages(searchTerm);
@@ -164,29 +106,27 @@ const useProducts = (initialPerPage = 10) => {
     toggleForm();
   };
 
-  const { uploadFile } = useCloudinaryUpload();
-
   const handleAddNewProduct = async (values, { resetForm }) => {
-    const productsRef = collection(firestore, ref);
+    const customId = Date.now();
+    const productRef = doc(firestore, `${ref}/${customId}`);
     let image = "";
     if (values.image) {
-      image = await uploadFile(values.image);
+      image = await uploadFile(values.image, ref, "image");
     }
     try {
-      const res = await addDoc(productsRef, {
+      await dbPOST(productRef, {
         ...values,
+        id: customId,
         image,
         price_after_discount: values.price - values.discount,
         created_at: serverTimestamp(),
       });
-      if (res.id) {
-        closeForm();
-        resetForm();
-        showToast({
-          title: "Sukses",
-          description: "Produk berhasil ditambahkan",
-        });
-      }
+      closeForm();
+      resetForm();
+      showToast({
+        title: "Sukses",
+        description: "Produk berhasil ditambahkan",
+      });
     } catch (error) {
       console.error("Gagal menambahkan produk:", error);
       showToast({
@@ -201,19 +141,15 @@ const useProducts = (initialPerPage = 10) => {
     const productRef = doc(firestore, ref, values.id);
     let image = values.image;
     if (values.image instanceof File || values.image instanceof Blob) {
-      image = await uploadFile(values.image);
+      image = await uploadFile(values.image, productRef, "image");
     }
     try {
-      await setDoc(
-        productRef,
-        {
-          ...values,
-          image,
-          price_after_discount: values.price - values.discount,
-          updated_at: serverTimestamp(),
-        },
-        { merge: true },
-      );
+      await dbUPDATE(productRef, {
+        ...values,
+        image,
+        price_after_discount: values.price - values.discount,
+        updated_at: serverTimestamp(),
+      });
       closeForm();
       resetForm();
       showToast({
@@ -241,7 +177,7 @@ const useProducts = (initialPerPage = 10) => {
 
     if (confirmed) {
       const productRef = doc(firestore, ref, product.id);
-      await deleteDoc(productRef)
+      await dbDELETE(productRef)
         .then(() => {
           showToast({
             title: "Sukses",
